2644cf38f48d152aef7d2696a5953ec3e4006a18
Mohsan Ali <foragerdev@protonmail.com>
2026-06-03 15:55:21 +0500

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.cpp
@@ -0,0 +1,35 @@
+#include <print>
+class Solution {
+public:
+
+    bool canEatAll(vector<int>& p, int rate, int h) {
+
+        int hours = 0;
+        for(int i = 0; i < p.size(); i++) {
+            if(p[i] < rate) hours++;
+            else {
+                hours += (p[i] / rate) + (p[i] % rate ? 1 : 0);
+            }
+        }
+        return hours <= h;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        long long start = 1;
+        long long end = 1000000000;
+
+        while(start <= end) {
+            auto mid = (start + end) / 2;
+            auto canEat = canEatAll(piles, mid, h);
+            std::println("Bananas at {} rate can{} be eaten", mid, canEat ? "" :"not ");
+            if(canEat) {
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+
+
+        return start;
+    }
+};
